b20cadb5d7bdf16f987e5adf2c39244d72ca117b
samdixon <sdixon@posteo.net>
2022-01-24 09:03:50 -0500

    add page

---
+++ pages/other.js
@@ -0,0 +1,34 @@
+import { withSSRContext} from 'aws-amplify';
+
+
+export default function other({props}) {
+    return (
+        <p>{JSON.stringify(props.data)}</p>
+    )
+}
+
+export async function getServerSideProps(context) {
+    var q = `query listSjds {
+      listSjds {
+        items {
+          FirstName
+        }
+      }
+    }`
+  
+    const { API } = withSSRContext(context)
+    try {
+      var data = await API.graphql({
+        query: q,
+        authMode: "AMAZON_COGNITO_USER_POOLS",
+      })  
+      console.log(data)
+    } catch(err) {
+      console.log(err)
+    }
+    return {
+      props: {
+        data: data
+      }
+    }
+  }
